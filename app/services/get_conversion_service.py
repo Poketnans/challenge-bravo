55e1b8fcbeb7fbe1d0257dd90582b1daa6c3b849
Etnan Sousa <poketnans@gmail.com>
2022-09-02 11:02:43 -0300

    refactor: uses separated logical cases

--- app/services/get_conversion_service.py
+++ app/services/get_conversion_service.py
@@ -1,24 +1,31 @@
 from datetime import datetime
 
 from app.classes.app_with_db import current_app as curr_app
-from app.services.create_or_update_service import create_or_update
-from app.services.get_indirect_crypto_cotation_service import (
-    get_indirect_crypto_cotation,
+from app.models.cotations_model import Cotation
+from app.services.get_crypto_to_crypto_cotation import get_crypto_to_crypto_cotation
+from app.services.get_crypto_to_non_usd_external_cotation import (
+    get_crypto_to_non_usd_external_cotation,
 )
-from app.services.get_indirect_internal_cotation import get_indirect_internal_cotation
-from app.utils import fetch, get_quote_date, get_rate
+from app.services.get_external_to_internal_cotation import (
+    get_external_to_internal_cotation,
+)
+from app.services.get_internal_to_internal_cotation import (
+    get_internal_to_internal_cotation,
+)
+from app.services.get_non_crypto_to_non_crypto_cotation import (
+    get_non_crypto_to_non_crypto_cotation,
+)
+from app.utils.get_cotation import get_cotation
 
 
 def get_conversion_service():
     from_currency = curr_app.from_currency
     to_currency = curr_app.to_currency
-    _from = from_currency.code
-    to = to_currency.code
     amount = curr_app.amount_param
-    conversion = 1
-    quote_date = datetime.now()
+    cotation: Cotation = curr_app.cotation
+    conversion = ...
 
-    if _from == to:
+    if from_currency.code == to_currency.code:
         msg = {"message": "Nothing to convert."}
         return msg
 
@@ -31,45 +38,56 @@ def get_conversion_service():
         conversion = (
             amount / cot_rate if curr_app.inverted_conversion else amount * cot_rate
         )
-        quote_date = curr_app.cotation.updated_at
 
+    # Crypto to Crypto
+    elif from_currency.is_crypto and to_currency.is_crypto:
+        cotation = get_crypto_to_crypto_cotation(from_currency, to_currency)
+        conversion = amount * cotation.rate
+
+    # External to Internal
+    elif from_currency.is_external and not to_currency.is_external:
+        print("weww")
+        cotation = get_external_to_internal_cotation(to_currency, from_currency)
+        conversion = amount * cotation.rate
+
+    # External to Internal Inverted
+    elif to_currency.is_external and not from_currency.is_external:
+        cotation = get_external_to_internal_cotation(to_currency, from_currency)
+        conversion = amount / cotation.rate
+
+    # Crypto to non usd external
     elif from_currency.is_crypto and not to_currency.backing_currency:
-        cotation = get_indirect_crypto_cotation(from_currency, to_currency)
+        cotation = get_crypto_to_non_usd_external_cotation(from_currency, to_currency)
         conversion = amount * cotation.rate
 
+    # Crypto to non usd external inverted
     elif to_currency.is_crypto and not from_currency.backing_currency:
-        cotation = get_indirect_crypto_cotation(to_currency, from_currency)
+        cotation = get_crypto_to_non_usd_external_cotation(to_currency, from_currency)
         conversion = amount / cotation.rate
 
-    elif not from_currency.is_external:
-        cotation = get_indirect_internal_cotation(from_currency, to_currency)
+    # Crypto to USD
+    elif from_currency.is_crypto and to_currency.backing_currency:
+        cotation = get_cotation(from_currency, to_currency)
         conversion = amount * cotation.rate
 
-    elif not to_currency.is_external:
-        cotation = get_indirect_internal_cotation(to_currency, from_currency)
+    # Crypto to USD inverted
+    elif to_currency.is_crypto and from_currency.backing_currency:
+        cotation = get_cotation(to_currency, from_currency)
         conversion = amount / cotation.rate
+
+    # Internal to Internal
+    elif not from_currency.is_external and not to_currency.is_external:
+        cotation = get_internal_to_internal_cotation(from_currency, to_currency)
+        conversion = amount * cotation.rate
 
     else:
-        conversion_data = fetch(_from, to)
-        quote_rate = float(get_rate(conversion_data))
-
-        rate = (1 / quote_rate) if curr_app.inverted_conversion else quote_rate
-        conversion = amount * rate
-
-        cotation = create_or_update(
-            from_currency,
-            to_currency,
-            rate,
-            get_quote_date(conversion_data),
-            curr_app.cotation,
-        )
-
-        quote_date = cotation.quote_date
+        cotation = get_non_crypto_to_non_crypto_cotation(from_currency, to_currency)
+        conversion = amount * cotation.rate
 
     payload = {
-        _from: round(amount, 4),
-        to: round(conversion, 4),
-        "quote_date": quote_date.strftime("%Y-%m-%d %H:%M:%S"),
+        from_currency.code: round(amount, 4),
+        to_currency.code: round(conversion, 4),
+        "quote_date": cotation.quote_date.strftime("%Y-%m-%d %H:%M:%S"),
     }
 
     return payload
